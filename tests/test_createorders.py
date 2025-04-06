a3e9a5c3d6932e02dbba0c2f7007e6b92b080244
Dylan <cristhian.butron@ucb.edu.bo>
2025-04-05 21:16:44 -0600

    Implemetacion test_send to kitchen completo con todos sus implementos falta correcciones

--- tests/test_createorders.py
+++ tests/test_createorders.py
@@ -42,13 +42,12 @@ def test_clear(create_orders):
     assert create_orders.tb_name_entry.get() == ""
     mock_order.destroy_all.assert_called_once()
 
-def test_send_to_kitchen_empty_table(create_orders):
+def test_send_to_kitchen(create_orders):
     with patch('tkinter.messagebox.showerror') as mock_error:
         result = create_orders.send_to_kitchen()
         assert result is None
         mock_error.assert_called_once_with("Empty Fields", "Please enter a valid table number!")
-
-def test_send_to_kitchen_success(create_orders):        
+     
     mock_order = MagicMock()
     mock_order.retrieve_data.return_value = ['Carne', '2']
     create_orders.order_ls = [mock_order]
