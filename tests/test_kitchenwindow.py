6fe36df1a4af5974c0982223eb4ee1e3b1f68f7d
Grover Gonzalez <grover.gonzalez@ucb.edu.bo>
2025-04-13 11:35:39 -0400

    agregando pruebas para la funcion retrieve_pr

--- tests/test_kitchenwindow.py
+++ tests/test_kitchenwindow.py
@@ -35,3 +35,18 @@ def test_add_widgets_empty_db(config_window, mock_db, mocker):
     mock_retrieve = mocker.patch.object(config_window, 'retrieve_pr')
     config_window.add_widgets()
     mock_retrieve.assert_not_called()
+
+#Test 1: Instancia OrderedProducts
+def test_retrieve_pr(config_window, mocker):
+    mock_ordered_products = mocker.patch('BASE.Components.kitchenwindow.OrderedProducts')
+    test_table_num = 5
+    config_window.retrieve_pr(test_table_num)
+    mock_ordered_products.assert_called_once_with(
+        config_window.main_frame,
+        config_window.nt,
+        config_window.kt_lb,
+        str(test_table_num),
+        config_window.destroy
+    )
+    assert config_window.op == mock_ordered_products.return_value
+    assert config_window.op == mock_ordered_products.return_value
